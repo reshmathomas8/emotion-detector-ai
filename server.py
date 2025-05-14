961df4a392ed76581100abee823046150c5c3064
Reshma Thomas <reshmathomas35@gmail.com>
2025-05-14 04:09:48 -0400

    add server file and unit tests

---
+++ server.py
@@ -0,0 +1,25 @@
+''' Executing this function initiates the application of emotion detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask pramework package : TODO
+from flask import Flask, render_template, request
+# Import the emotion detector function from the package created
+from EmotionDetection.emotion_detection import emotion_detector
+
+#Initiate the flask app 
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def emotion_detection():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detector(text_to_analyze)
+    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(response["anger"], response["disgust"], response["fear"], response["joy"], response["sadness"], response["dominant_emotion"])
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
